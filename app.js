509647eccd2f5d3b5b2100148fbcd12c4e2eefad
Procsimo <teterina.anya@yandex.ru>
2024-01-24 21:58:51 +0800

    настройка cors

--- app.js
+++ app.js
@@ -8,13 +8,26 @@ const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 
+const options = {
+  origin: [
+    'http://localhost:3000',
+    // 'https://ВАШ ДОМЕЙН С ДОКУМЕНТА',
+    // 'https://YOUR.github.io',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
 const { DB_ADDRESS, NODE_ENV } = process.env;
 
 const routes = require('./routes/index');
 
 const PORT = 3000;
 const app = express();
-app.use(cors());
+app.use(cors(options));
 
 mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://127.0.0.1:27017/movie')
   .catch((err) => {
